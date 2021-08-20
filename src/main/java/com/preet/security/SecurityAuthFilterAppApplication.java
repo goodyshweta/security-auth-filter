7892b0110a815c491de603ca7e173968c49239cf
gurpreet <get2gpreet@gmail.com>
2021-08-20 22:46:40 +0530

    initial repo creation

---
+++ src/main/java/com/preet/security/SecurityAuthFilterAppApplication.java
@@ -0,0 +1,13 @@
+package com.preet.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAuthFilterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAuthFilterAppApplication.class, args);
+	}
+
+}
