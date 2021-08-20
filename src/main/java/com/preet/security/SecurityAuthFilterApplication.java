781e655d801a547acdeb94e4a298e969c21fcf1f
gurpreet <get2gpreet@gmail.com>
2021-08-20 22:50:28 +0530

    reformatting

--- src/main/java/com/preet/security/SecurityAuthFilterApplication.java
+++ src/main/java/com/preet/security/SecurityAuthFilterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityAuthFilterAppApplication {
+public class SecurityAuthFilterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityAuthFilterAppApplication.class, args);
+		SpringApplication.run(SecurityAuthFilterApplication.class, args);
 	}
 
 }
